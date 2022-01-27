2cbb801fa56a1b6cd4fe912b6221858de56c98e5
Dasha <darya.lynnyk@juanxxiii.net>
2022-01-27 20:13:12 +0100

    Make dinamically index by Raul Gogna

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 window.onload = cargarAjax;
+
+
 let reproductor = document.querySelector("#track");
 let musica = [];
 let sonando = false;
@@ -15,21 +17,30 @@ function cargarAjax() {
                 console.log("dentro*2");
                 let datos = req.responseText;
                 musica = JSON.parse(datos);
+                let principal = document.querySelector('#principal');
                 //console.log(canciones);
                 musica.forEach(grupo => {
+                    let div = document.createElement('div');
+                    let a = document.createElement('a');
+                    let img = document.createElement('img');
                     let songs = grupo.songs;
                     let artistaElement = document.querySelector('#artista');
                     let cancionElement = document.querySelector('#cancion');
                     artistaElement.textContent = grupo.artista;
                     cancionElement.textContent = grupo.songs[0]['titulo'];
-                    document.querySelector('#lista-canciones').appendChild(titulo);
-                    titulo.addEventListener("click", () => {
+                    a.href = 'always.html?id=' + grupo.id;
+                    img.src = './assets/' + grupo.imagen;
+                    a.addEventListener("click", () => {
                         //document.querySelector('#cancionSeleccionada').innerHTML = cancion.titulo;
                         //cargarCancion(cancion.titulo, cancion.cancion);
                         console.log(songs);
                         cargarCancion(songs['id']);
                         console.log(songs['id']);
                     });
+
+                    a.appendChild(img);
+                    div.appendChild(a);
+                    principal.appendChild(div);
                 });
             }
         }
@@ -91,7 +102,7 @@ function cargar() {
 }
 
 function reproducir_pausar() {
-    if(!sonando){
+    if (!sonando) {
         console.log("sonando");
         reproductor.play();
         sonando = true;
