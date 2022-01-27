024719b0a26c0ac3982061b58b8e61554772f3c4
Dasha <darya.lynnyk@juanxxiii.net>
2022-01-27 20:44:26 +0100

    Cambios varios por Raul Gogna/Dasha

--- always.php
+++ always.php
@@ -1,3 +1,10 @@
+<?php
+$p = null;
+if(isset($_GET['id']))
+$p = $_GET['id'];
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,8 +14,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GOGNA MUSIC</title>
     <link href='https://fonts.googleapis.com/css?family=Righteous' rel='stylesheet'>
-    <link rel="stylesheet" href="../css/estilos.css">
-    <link rel="stylesheet" href="../assets/fontawesome-free-5.15.4-web/css/all.css">
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="assets/fontawesome-free-5.15.4-web/css/all.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 
@@ -17,7 +24,7 @@
     <header>
         <nav>
             <div class="nombre">
-                <img src="../assets/imgs/pera.jpg" class="icono" alt="">
+                <img src="assets/imgs/pera.jpg" class="icono" alt="">
                 <h3 class="titulo">GOGNA MUSIC</h3>
             </div>
         </nav>
@@ -32,9 +39,9 @@
                 <div class="repro">
                     <audio src="" id="track"></audio>
                     <input type="range" name="" id="barra" value="0" step="any">
-                    <button class="botons" type="button" id="rebobinar"><img src="../assets/imgs/rebobinar.png" class="icons-repro"></button>
-                    <button class="botons" type="button" id="play"><img src="../assets/imgs/play.png" class="icons-repro"></button>
-                    <button class="botons" type="button" id="avanzar"><img src="../assets/imgs/avance-rapido.png" class="icons-repro"></button>
+                    <button class="botons" type="button" id="rebobinar"><img src="assets/imgs/rebobinar.png" class="icons-repro"></button>
+                    <button class="botons" type="button" id="play"><img src="assets/imgs/play.png" class="icons-repro"></button>
+                    <button class="botons" type="button" id="avanzar"><img src="assets/imgs/avance-rapido.png" class="icons-repro"></button>
                     <input type="range" name="" id="volumen" value="0,5" min="0" max="1" step="any">
                 </div>
             </div>
@@ -50,43 +57,43 @@
                         </form>
                     </div>
                     <div class="escuchar mt-3">
-                        <img src="../assets/iconos-sara/play-circle-fill.svg" alt="">
+                        <img src="assets/iconos-sara/play-circle-fill.svg" alt="">
                         <span>Escuchar</span>
                     </div>
                     <div class="explorar mt-3">
-                        <img src="../assets/iconos-sara/binoculars-fill.svg" alt="">
+                        <img src="assets/iconos-sara/binoculars-fill.svg" alt="">
                         <span>Explorar</span>
                     </div>
                     <div class="radio mt-3">
-                        <img src="../assets/iconos-sara/ear-fill.svg" alt="">
+                        <img src="assets/iconos-sara/ear-fill.svg" alt="">
                         <span>Radio</span>
                     </div>
                     <div class="biblioteca mt-3">
                         <h4 class="mt-3">Biblioteca</h2>
                             <hr class="my-4">
                             <div class="added mt-3">
-                                <img src="../assets/iconos-sara/clock.svg" alt="">
+                                <img src="assets/iconos-sara/clock.svg" alt="">
                                 <span>Añadido recientemente</span>
                             </div>
                             <div class="artistas mt-3">
-                                <img src="../assets/iconos-sara/mic.svg" alt="">
+                                <img src="assets/iconos-sara/mic.svg" alt="">
                                 <span>Artistas</span>
                             </div>
                             <div class="albumes mt-3">
-                                <img src="../assets/iconos-sara/music-note-list.svg" alt="">
+                                <img src="assets/iconos-sara/music-note-list.svg" alt="">
                                 <span>Álbumes</span>
                             </div>
                             <div class="canciones mt-3">
-                                <img src="../assets/iconos-sara/file-music.svg" alt="">
+                                <img src="assets/iconos-sara/file-music.svg" alt="">
                                 <span>Canciones</span>
                             </div>
                     </div>
                 </div>
                 <div class="principal col-8 mt-4">
-                    <div class="portada container">
+                    <div class="portada container" id="portada">
                         <div class="row m-3">
                             <div class="col">
-                                <img class="col-10" src="../assets/imgs-albumes/alwaysfeelthecold.jpg" alt="">
+                                <img class="col-10 imgPortada" alt="" id="<?=$p ?>">
                             </div>
                             <div class="col">
                                 <h2 class="titulo-album">Always Feel the Cold</h1>
@@ -100,14 +107,14 @@
                         </div>
                     </div>
                     <div class="lista-canciones col mt-5 m-3">
-                        <ol>
+                        <!-- <ol>
                             <li class="m-3" id="always-1">Always Feel the Cold</li>
                             <li class="m-3" id="always-2">Big Sleep</li>
                             <li class="m-3" id="always-3">C.R.Y.</li>
                             <li class="m-3" id="always-4">Dead Dreams</li>
                             <li class="m-3" id="always-5">Everlasting Room</li>
                             <li class="m-3" id="always-6">Somebody Kill Me</li>
-                        </ol>
+                        </ol> -->
                     </div>
                     <div class="volver m-5">
                         <button class="btn"><a href="../index.html">Volver</a></button>
@@ -116,7 +123,7 @@
             </div>
         </div>
     </section>
-    <script src="js/script.js"></script>
+    <script src="js/albumes.js"></script>
 </body>
 
 </html>
--- js/albumes.js
+++ js/albumes.js
@@ -1,6 +1,5 @@
 window.onload = cargarAjax;
 
-
 let reproductor = document.querySelector("#track");
 let musica = [];
 let sonando = false;
@@ -17,29 +16,34 @@ function cargarAjax() {
                 console.log("dentro*2");
                 let datos = req.responseText;
                 musica = JSON.parse(datos);
-                let principal = document.querySelector('#principal');
+                let imgPortadaID = document.querySelector('.imgPortada').getAttribute('id');
+                let imgPortadaSrc = document.querySelector('.imgPortada');
+                console.log(imgPortadaSrc);
                 musica.forEach(grupo => {
-                    let div = document.createElement('div');
-                    let a = document.createElement('a');
-                    let img = document.createElement('img');
-                    let songs = grupo.songs;
-                    let artistaElement = document.querySelector('#artista');
-                    let cancionElement = document.querySelector('#cancion');
-                    artistaElement.textContent = grupo.artista;
-                    cancionElement.textContent = grupo.songs[0]['titulo'];
-                    a.href = 'always.php?id=' + grupo.id;
-                    img.src = './assets/' + grupo.imagen;
-                    a.addEventListener("click", () => {
-                        //document.querySelector('#cancionSeleccionada').innerHTML = cancion.titulo;
-                        //cargarCancion(cancion.titulo, cancion.cancion);
-                        console.log(songs);
-                        cargarCancion(songs['id']);
-                        console.log(songs['id']);
-                    });
+                    if (imgPortadaID == grupo.id) {
+                        imgPortadaSrc.src = "./assets/" + grupo.imagen;
+                    }
+                    //     let div = document.createElement('div');
+                    //     let a = document.createElement('a');
+                    //     let img = document.createElement('img');
+                    //     let songs = grupo.songs;
+                    //     let artistaElement = document.querySelector('#artista');
+                    //     let cancionElement = document.querySelector('#cancion');
+                    //     artistaElement.textContent = grupo.artista;
+                    //     cancionElement.textContent = grupo.songs[0]['titulo'];
+                    //     a.href = 'always.html?id=' + grupo.id;
+                    //     img.src = './assets/' + grupo.imagen;
+                    //     a.addEventListener("click", () => {
+                    //         //document.querySelector('#cancionSeleccionada').innerHTML = cancion.titulo;
+                    //         //cargarCancion(cancion.titulo, cancion.cancion);
+                    //         console.log(songs);
+                    //         cargarCancion(songs['id']);
+                    //         console.log(songs['id']);
+                    //     });
 
-                    a.appendChild(img);
-                    div.appendChild(a);
-                    principal.appendChild(div);
+                    //     a.appendChild(img);
+                    //     div.appendChild(a);
+                    //     principal.appendChild(div);
                 });
             }
         }
